92a81de91152f0ee5189899ba73b46eb7821cd61
bharadwaj2929 <89787069+bharadwaj2929@users.noreply.github.com>
2021-12-28 17:58:54 +0530

    Create hrDepartment.java

---
+++ hrDepartment.java
@@ -0,0 +1,35 @@
+package com.greatlearning.hrdepartment;
+
+import com.greatlearning.superdepartment.superDepartment;
+
+public class hrDepartment extends superDepartment{
+	
+	public String departmentName(){
+		
+		String departmentName="HR Department";
+		return departmentName;
+		
+	}
+	
+	public String getTodaysWork(){
+		
+		String todaysWork="Fill Today's Worksheets And Mark Your Attendance";
+		return todaysWork;
+		
+	}
+	
+	public String getWorkDeadline(){
+		
+		String workDeadline="Complete By EOD";
+		return workDeadline;
+		
+	}
+	
+	public String doActivity(){
+		
+		String mission="Team Lunch";
+		return mission;
+		
+	}
+	
+}
