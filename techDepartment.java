93343ce7fa7fb51c2b9b02fd2fbbb9af1e0ddde9
bharadwaj2929 <89787069+bharadwaj2929@users.noreply.github.com>
2021-12-28 18:00:20 +0530

    Create techDepartment.java

---
+++ techDepartment.java
@@ -0,0 +1,35 @@
+package com.greatlearning.techdepartment;
+
+import com.greatlearning.superdepartment.superDepartment;
+
+public class techDepartment extends superDepartment{
+	
+	public String departmentName(){
+		
+		String departmentName="Tech Department";
+		return departmentName;
+		
+	}
+	
+	public String getTodaysWork(){
+		
+		String todaysWork="Complete Coding Of Module1";
+		return todaysWork;
+		
+	}
+	
+	public String getWorkDeadline(){
+		
+		String workDeadline="Complete By EOD";
+		return workDeadline;
+		
+	}
+	
+	public String getTechStackInformation(){
+		
+		String techStackInformation="Core Java";
+		return techStackInformation;
+		
+	}
+	
+}
