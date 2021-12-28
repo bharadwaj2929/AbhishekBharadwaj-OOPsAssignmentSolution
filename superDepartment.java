4ff2d5d7ff4d39ffde1f4b3669176bcc8866f70e
bharadwaj2929 <89787069+bharadwaj2929@users.noreply.github.com>
2021-12-28 17:59:31 +0530

    Create superDepartment.java

---
+++ superDepartment.java
@@ -0,0 +1,33 @@
+package com.greatlearning.superdepartment;
+
+public class superDepartment{
+	
+	public String departmentName(){
+		
+		String department="Super Department";
+		return department;
+	
+	}
+	
+	public String getTodaysWork(){
+		
+		String todaysWork="No Work As Of Now";
+		return todaysWork;
+		
+	}
+	
+	public String getWorkDeadline(){
+		
+		String workDeadline="Nil";
+		return workDeadline;
+		
+	}
+	
+	public String isTodayAHoliday(){
+		
+		String todayHoliday="Today'sn't A Holiday";
+		return todayHoliday;
+		
+	}
+	
+}
